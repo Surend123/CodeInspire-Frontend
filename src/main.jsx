5fd8989816b7f22b6ecbc54e79e2cb6c0a24a001
Surend123 <sky958447@gmail.com>
2024-09-11 10:51:06 +0530

    hosting web backend link add

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://codeinspire-server.onrender.com";
+export const server = "https://95e758a1-4eef-4682-a69a-cc2c344d72f1.e1-us-cdp-2.choreoapps.dev";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
